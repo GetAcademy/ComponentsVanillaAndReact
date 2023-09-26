f082e9b22222b3002b794085331131a1ceb1b706
GET Academy <terje@getacademy.no>
2023-09-26 15:39:11 +0200

    x

--- react-click-counter/src/App.js
+++ react-click-counter/src/App.js
@@ -4,14 +4,14 @@ import ClickCounter from './ClickCounter';
 
 
 function App() {
-  const handleTriggerValueReached = (event) => {
-    console.log(`Telleren nådde: ${event.detail.newValue}`);
+  const handleTriggerValueReached = (value) => {
+    console.log(`Telleren nådde: ${value}`);
   };
 
   return (
     <div className="App">
-      <ClickCounter startValue={10} triggerValue={20} onTriggerValueReached={handleTriggerValueReached}></ClickCounter>
-      <ClickCounter startValue={5} triggerValue={10} onTriggerValueReached={handleTriggerValueReached}></ClickCounter>
+      <ClickCounter startValue={10} triggerValue={20} triggerValueReached={handleTriggerValueReached}></ClickCounter>
+      <ClickCounter startValue={5} triggerValue={10} triggerValueReached={handleTriggerValueReached}></ClickCounter>
     </div>
   );
 }
--- react-click-counter/src/ClickCounter.js
+++ react-click-counter/src/ClickCounter.js
@@ -4,17 +4,14 @@ class ClickCounter extends Component {
     constructor(props) {
         super(props);
         this.state = { value: props.startValue, triggerValue: props.triggerValue };
+        this.triggerValueReached = props.triggerValueReached;
     }
 
     handleIncrement = () => {
         this.setState(prevState => {
             const state = { ...prevState, value: prevState.value + 1 };
             if (state.value >= state.triggerValue) {
-                console.log('trigger');
-                const event = new CustomEvent('triggerValueReached', {
-                    detail: { value: state.value },
-                });
-                this.counterElement.dispatchEvent(event);
+                this.triggerValueReached(state.value);
             }
             return state;
         });
