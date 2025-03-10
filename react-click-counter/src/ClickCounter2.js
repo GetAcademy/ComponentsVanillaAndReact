f534b8910deb256f4b2995dfd30b5984af4662c1
GET Academy <terje@kolderup.net>
2025-03-10 10:14:45 +0100

    Update ClickCounter2.js

--- react-click-counter/src/ClickCounter2.js
+++ react-click-counter/src/ClickCounter2.js
@@ -8,13 +8,14 @@ function ClickCounter2({ startValue, triggerValue, onTriggerValueReached }) {
     if (value >= triggerValue) {
       onTriggerValueReached(value);
     }
-  });
+  }, [value, triggerValue, onTriggerValueReached]);
 
   return (
     <div>
       <p>Teller: {value}</p>
       <p>Teller 2: {value2}</p>
-      <button onClick={setValue(value + 1)}>Øk telleren</button>
+      <button onClick={()=>setValue(value + 1)}>Øk teller 1</button>
+      <button onClick={()=>setValue2(value2 + 1)}>Øk teller 2</button>
     </div>
   );
 }
